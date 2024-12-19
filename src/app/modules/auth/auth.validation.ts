a0bba95df9f17000f512dfdb463da3a2ba36194c
riasat01 <riasat01@outlook.com>
2024-12-19 21:31:27 +0600

    feat: create user login interface and validation schema - Added user login interface for type safety and consistency. - Implemented validation schema to ensure proper input handling. - Enhanced overall authentication module with new features.

---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const userLoginValidationSchema = z.object({
+    body: z.object({
+        email: z.string({
+            required_error: `User email is required`,
+            invalid_type_error: `User email must be a string`,
+        }),
+        passwrod: z.string({
+            required_error: `Password is required`,
+            invalid_type_error: `Password must be a string`,
+        }),
+    }),
+});
+
+export const AuthValidations = {
+    userLoginValidationSchema
+};
